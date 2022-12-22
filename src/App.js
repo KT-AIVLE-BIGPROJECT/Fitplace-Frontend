0ca64328b79949c45d06f7a6a2214d75e5b8c763
jaesso4245 <jeongyun4245@naver.com>
2022-12-22 15:03:31 +0900

    22.12.22_so

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 import Main from './Main/Main';
 import SignUp from './pages/SignUp';
 import Board from './pages/Board';
+import Board_detail from './Board/Board_detail';
+import Board_edit from './Board/Board_edit';
+import Board_write from './Board/Board_write';
+import Board_page from './Board/Board_page';
 import Search from './pages/Search';
 import BeforeSignUp from './pages/BeforeSignUp';
 import Profile from './pages/Profile';
 import ProfileModify from './pages/ProfileModify';
+
 
 function App() {
   return (
@@ -16,6 +21,10 @@ function App() {
         <Route path="/" element={<Main/>}></Route>
         <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/board" element={<Board/>}></Route>
+        <Route path="/board/detail" element={<Board_detail/>}></Route>
+        <Route path="/board/edit" element={<Board_edit/>}></Route>
+        <Route path="/board/write" element={<Board_write/>}></Route>
+        <Route path="/board/page" element={<Board_page/>}></Route>
         <Route path="/search" element={<Search/>}></Route>
         <Route path="/bfsignup" element={<BeforeSignUp/>}></Route>
         <Route path="/profile" element={<Profile/>}></Route>
--- src/Board/Board_main.js
+++ src/Board/Board_main.js
@@ -1,75 +1,44 @@
 import React from 'react'
 import "./Board_main.css"
+import "./Board_style.css"
 
 const Board_main = () => {
   return (
     <div class="board_container">
-        <div class="board_row">
-            <div class="col">
-                <table class="table-fill">
-                <thead>
-                    <tr>
-                    <th class="text-center">글번호</th>
-                    <th class="text-center">제목</th>
-                    <th class="text-center">글쓴이</th>
-                    <th class="text-center">날짜</th>
-                    <th class="text-center">조회수</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td class="text-center">1</td>
-                    <td class="text-center">질문이요</td>
-                    <td class="text-center">홍길동</td>
-                    <td class="text-right">2202-12-20</td>
-                    <td class="text-center">1</td>
-                    </tr>
-                    <tr>
-                    <td class="text-center">1</td>
-                    <td class="text-center">질문이요</td>
-                    <td class="text-center">홍길동</td>
-                    <td class="text-right">2202-12-20</td>
-                    <td class="text-center">1</td>
-                    </tr>
-                    <tr>
-                    <td class="text-center">1</td>
-                    <td class="text-center">질문이요</td>
-                    <td class="text-center">홍길동</td>
-                    <td class="text-right">2202-12-20</td>
-                    <td class="text-center">1</td>
-                    </tr>
-                    <tr>
-                    <td class="text-left">1</td>
-                    <td class="text-center">질문이요</td>
-                    <td class="text-center">홍길동</td>
-                    <td class="text-right">2202-12-20</td>
-                    <td class="text-center">1</td>
-                    </tr>
-                    <tr>
-                    <td class="text-left">1</td>
-                    <td class="text-center">질문이요</td>
-                    <td class="text-center">홍길동</td>
-                    <td class="text-right">2202-12-20</td>
-                    <td class="text-center">1</td>
-                    </tr>
-                    <tr>
-                    <td class="text-left">1</td>
-                    <td class="text-center">질문이요</td>
-                    <td class="text-center">홍길동</td>
-                    <td class="text-right">2202-12-20</td>
-                    <td class="text-center">1</td>
-                    </tr>
-                    
-                </tbody>
-                <tfoot>
-                    
-                </tfoot>
-                </table>
-                <section class="content">
-                    <div class="download">
-                        <a class="button" href="https://github.com/victordarras/HeavyTable.js"> 글쓰기</a>
-                    </div>
-                </section>
+        <h1>FAQ</h1>
+        <p>질문을 남겨주세요.</p>
+        
+        <div class="board_list_wrap">
+            <div class="board_list">
+                <div class="top">
+                    <div class="num">번호</div>
+                    <div class="title">제목</div>
+                    <div class="writer">글쓴이</div>
+                    <div class="date">작성일</div>
+                    <div class="count">조회</div>
+                </div>
+                <div>
+                    <div class="num">1</div>
+                    <div class="title"><a href="/board/detail">글</a></div>
+                    <div class="writer">홍길동</div>
+                    <div class="date">2022-02-02</div>
+                    <div class="count">1</div>
+                </div>
+                
+            </div>
+            <div class="board_page">
+                <a href="#" class="bt first">&lt;&lt;</a>
+                <a href="#" class="bt prev">&lt;</a>
+                <a href="#" class="num on">1</a>
+                <a href="#" class="num">2</a>
+                <a href="#" class="num">3</a>
+                <a href="#" class="num">4</a>
+                <a href="#" class="num">5</a>
+                <a href="#" class="bt next">&gt;</a>
+                <a href="#" class="bt last">&gt;&gt;</a>
+            </div>
+            <div class="bt_wrap">
+                <a href="/board/write" class="on">글쓰기</a>
             </div>
         </div>
     </div>
