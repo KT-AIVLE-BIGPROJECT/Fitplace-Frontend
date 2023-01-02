4a131fe97d09ac9958a9a21e8ac4fb186552bad7
kybeen <rei980627@gmail.com>
2023-01-03 03:03:18 +0900

    (23.01.02) - 댓글 구현 중

--- src/Board/Board_detail.js
+++ src/Board/Board_detail.js
@@ -6,63 +6,118 @@ import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import Footer from '../layouts/Footer';
-
-// const token = sessionStorage.getItem("token");
+import { addComment } from '@babel/types';
 
 const Board_detail = () => {
     const [ data, setData ] = useState({});
     const location = useLocation();
+    const getPath = location.pathname.split('/')
+
+    const [nickname, setNickname] = useState("");
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
-    const [pk, setPk] = useState("");
+    const [pk, setPk] = useState(getPath[getPath.length-2]); // URL에서 게시글 id 받아옴
     const [results, setResults] = useState([]);
-    const [comment, setComment] = useState([]);
+    const [comment, setComment] = useState(""); // 작성하는 댓글
+    const [commentList, setCommentList] = useState([]); // 불러오는 댓글
+    const [thisCommentList, setThisCommentList] = useState([]);
     const [published_date, setPublished_date] = useState("");
     const [image, setImage] = useState("");
 
-    var link =  document.location.pathname.replace('/board/detail/','');
-    var modify = '/board/edit/' + link
+    const token = sessionStorage.getItem("token");
 
+    // var link =  document.location.pathname.replace('/board/detail/','');
+    var modify = '/board/edit/' + pk;
+    // const date = published_date.split('T')
+
+    // ------------------- [ 컴포넌트 ] -------------------
+    const LoadComments = () => {
+        if(commentList != []){
+            return (
+                <div>
+                    댓글 목록
+                    {thisCommentList.map((comment, idx) => {
+                        return (
+                            <div key={idx}>
+                                <div>
+                                    작성자 : {comment.profile.nickname}
+                                </div>
+                                <div>
+                                    내용 : {comment.text}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        }
+    }
+
+    // ------------------- [ 함수 ] -------------------
+    const addComment = (event) => {
+        event.preventDefault();
+        let commentForm = {
+            "post": pk,
+            "text": comment
+        }
+        axios.post(`http://localhost:8000/comments/`, commentForm, {
+            headers: {
+                "Authorization": `Token ${token}`
+            }
+        })
+        .then((response) => {
+            if(response.status < 300){
+                alert("댓글이 등록되었습니다.");
+                window.location.replace(`http://localhost:3000/board/detail/${pk}/`);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+    // 해당 게시글에 대한 댓글만 필터링 해주는 함수
+    const commentFiltering = () => {
+        var arr = commentList;
+        var new_arr = [];
+        for(var i=0; i<arr.length; i++){
+            if(arr[i].post === pk){
+                console.log(arr[i].post, pk);
+                new_arr.push(arr[i]);
+            }
+        }
+        setThisCommentList(new_arr);
+        console.log(thisCommentList);
+    };
+
+    // ------------------- [ useEffect ] -------------------
     useEffect(()=>{
+        console.log("AAA", pk);
         axios
-          .get(`http://localhost:8000/posts/${link}/`, {
+          .get(`http://localhost:8000/posts/${pk}/`, {
           })
           .then((response)=>{
-            console.log(link);
+            setNickname(response.data.profile.nickname);
             setPk(response.data.pk);
             setTitle(response.data.title);
             setBody(response.data.body);
             setPublished_date(response.data.published_date);
             setImage(response.data.image);
-  
           })
       },[])
-
-      const date = published_date.split('T')
-
-    //   const comments = {
-    //     title: title,
-    //     body: body,
-     
-    //   }
-
-    //   const Comment_button = async({comments}) => {
-    //     axios.put(`http://localhost:8000/posts/${link}/`,
-    //         comments,
-    //         //     {
-    //         //     headers: {
-    //         //         "Authorization": `Token ${token}`
-    //         //     },
-    //         //   }
-    //         )
-    //         .then((response) => {
-    //             if(response.status < 300) {
-    //               alert("등록이 완료되었습니다!");
-    //               window.location.replace(`http://localhost:8000/posts/${link}/`);
-    //             }
-    //           })
-    //     }
-
+    useEffect(()=>{
+    axios
+        .get('http://localhost:8000/comments/')
+        .then((response)=>{
+            console.log(response);
+            setCommentList(response.data.results);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, [])
+    useEffect(()=>{
+    commentFiltering();
+    }, [commentList])
 
     return (
     <Layout>
@@ -83,11 +138,11 @@ const Board_detail = () => {
                     </dl>
                     <dl>
                         <dt>글쓴이</dt>
-                        <dd>익명</dd>
+                        <dd>{nickname}</dd>
                     </dl>
                     <dl>
                         <dt>작성일</dt>
-                        <dd>{date[0]}</dd>  
+                        <dd>{published_date.substr(0,10)}</dd>  
                     </dl>
                 </div>
                 <div class="cont">
@@ -97,13 +152,16 @@ const Board_detail = () => {
             <div>
                 <a href = {image} target="_blank"> 첨부파일 확인</a>
             </div>
-            {/* <div class="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">댓글</label>
-                            <button className="board_write_button" onClick={() => Comment_button({comments})}>
-                            댓글 등록
-                            </button>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={comment} onChange={(event) => setComment(event.target.value)}></textarea>
-                        </div> */}
+            <LoadComments></LoadComments>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">댓글 작성</label>
+                <div>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={comment} onChange={(event) => setComment(event.target.value)}></textarea>
+                    <button onClick={addComment} className="board_write_button">
+                        댓글 등록
+                    </button>
+                </div>
+            </div>
             <div class="bt_wrap">
                 <a href="/board" class="on">목록</a>
                 <a class = "board_cancel_button" href={modify}>수정</a>
